2fe7b2081013e2780472ad0f32c488bc1b9268c6
Andre <andre.fabian.melo@gmail.com>
2021-08-23 14:20:33 -0300

    Add auth middleware with tests

--- src/models/beach.ts
+++ src/models/beach.ts
@@ -1,4 +1,8 @@
-import mongoose, { Document as MongooseDocument, Model } from 'mongoose';
+import mongoose, {
+  Document as MongooseDocument,
+  Model,
+  Schema,
+} from 'mongoose';
 
 export enum BeachPosition {
   S = 'S',
@@ -21,6 +25,7 @@ const schema = new mongoose.Schema(
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     toJSON: {
