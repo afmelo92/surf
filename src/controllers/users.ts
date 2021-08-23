1baeb5b8325ac1e84f28505493803c766650f665
Andre <andre.fabian.melo@gmail.com>
2021-08-23 12:44:36 -0300

    Add auth service with tests

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 
@@ -19,5 +20,30 @@ export class UsersController extends BaseController {
         err as mongoose.Error.ValidationError | Error
       );
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user) {
+      return res.status(401).send({ code: 401, error: 'User not found!' });
+    }
+
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      return res
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' });
+    }
+
+    const token = AuthService.generateToken(user.toJSON());
+
+    return res.status(200).send({ token });
   }
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import config from 'config';
+import jwt from 'jsonwebtoken';
 
 export default class AuthService {
   public static async hashPassword(
@@ -13,5 +15,12 @@ export default class AuthService {
     hashedPassword: string
   ): Promise<boolean> {
     return bcrypt.compare(password, hashedPassword);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  public static generateToken(payload: object): string {
+    return jwt.sign(payload, config.get('App.auth.key'), {
+      expiresIn: config.get('App.auth.tokenExpiresIn'),
+    });
   }
 }
